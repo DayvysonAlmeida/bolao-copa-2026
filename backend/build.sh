6bd1794eadb640daf3f1907b252e2db3786d6ce5
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2026-06-10 00:27:38 -0300

    usuário admin variavel

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 echo "Rodando migrações no banco de dados..."
 python manage.py migrate
+
+echo "Sincronizando dados da Copa..."
+python manage.py sync_copa
+
+echo "Criando superusuário (se as variáveis de ambiente existirem)..."
+# O '|| true' evita que o script quebre se o usuário já existir em deploys futuros
+python manage.py createsuperuser --noinput || true
